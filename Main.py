e63f88d5b7a45e9512a490bde70a7ee3143913b3
Breno Gonçalves <124218590+Breno-G@users.noreply.github.com>
2025-05-30 17:26:47 -0300

    Update Main.py
    Últimas teste

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from carta import Carta, Naipe, Valores  # seus Enums
-from print_card_copy  import imprime_espaco_vazio
+from print_card_copy  import imprime_espacos_vazios
+from print_card_copy import imprime_carta_inteira
+from print_card_copy import imprime_pilha_empilhada
 
 NAIPES = list(Naipe)
 VALORES = list(Valores)
@@ -7,6 +9,11 @@ VALORES = list(Valores)
 baralho = [Carta(valor, naipe) for naipe in NAIPES for valor in VALORES]
 
 carta = Carta(Valores.AS, Naipe.OUROS)
-imprime_espaco_vazio(carta)
+#imprime_espacos_vazios()
+#imprime_carta_inteira(carta)
 
+pilha_exemplo = [Carta(valor, Naipe.ESPADAS) for valor in reversed(Valores)]
 
+imprime_pilha_empilhada(pilha_exemplo)
+        
+        
