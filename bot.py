69845d25935cea870c2eff910047102e6e431b23
Breno Gonçalves <124218590+Breno-G@users.noreply.github.com>
2025-06-12 17:33:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,10 @@ class Bot:
 
     def receber_cartas(self, cartas):
         self.mao.extend(cartas)
-        self.organizar_mao()
-
-    def organizar_mao(self):
-        ordem_naipe = [Naipe.ESPADAS, Naipe.COPAS, Naipe.PAUS, Naipe.OUROS]
-        self.mao.sort(key=lambda carta: (ordem_naipe.index(carta.naipe), -carta.valor.value))
+        
 
     def jogar(self, mesa):
         for carta in self.mao:
             if mesa.jogar_carta(carta):
                 self.mao.remove(carta)
-                self.organizar_mao()
                 return
